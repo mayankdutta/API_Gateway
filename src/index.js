49926c07a2e1b7899c32a17d1f571756ff4232a9
mayank <mayank@ubuntu.com>
2023-08-15 03:25:07 +0530

    authenticated a route.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const { createProxyMiddleware } = require("http-proxy-middleware");
 const rateLimit = require("express-rate-limit");
+const axios = require("axios");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
@@ -15,6 +16,37 @@ app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// app.get("http://localhost:3001/api/v1/isadmin/", async (req, res, next) => {});
+
+app.use("/bookingservice", async (req, res, next) => {
+  try {
+    const response = await axios.get(
+      "http://localhost:3001/api/v1/isauthenticated",
+      {
+        headers: {
+          "x-access-token": req.headers["x-access-token"],
+        },
+      }
+    );
+
+    console.log(response.data);
+    if (response.data.success) {
+      next();
+    } else {
+      return res.status(500).json({
+        err: response.data.err,
+        message: response.data.message,
+        success: response.data.success,
+        data: response.data.data,
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: "something went wrong",
+    });
+  }
+});
+
 app.use(
   "/bookingservice",
   createProxyMiddleware({
@@ -22,7 +54,6 @@ app.use(
     changeOrigin: true,
   })
 );
-
 
 app.get("/home", (req, res) => {
   return res.json({
