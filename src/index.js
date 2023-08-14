90cfdf92e92f067410f2a48dd54fa0e89bb324ed
mayank <mayank@ubuntu.com>
2023-08-15 02:20:15 +0530

    rate limiter, proxy implementd.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
 const PORT = 3005;
 
+const limiter = rateLimit({ windowMs: 2 * 60 * 1000, max: 5 });
+
 app.use(morgan("combined"));
+app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  "/bookingservice",
+  createProxyMiddleware({
+    target: "http://localhost:3002/",
+    changeOrigin: true,
+  })
+);
+
 
 app.get("/home", (req, res) => {
   return res.json({
